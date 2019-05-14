f1ca84d41f13d3e76e18b77225d8396cb5713345
giangson0208 <giangson0208@gmail.com>
2019-05-14 21:46:03 +0700

    add ErrorServce and fix PostCategoryController

---
+++ GS_Shop.Service/ErrorService.cs
@@ -0,0 +1,36 @@
+﻿using GS_Shop.Data.Infrastructure;
+using GS_Shop.Data.Repositories;
+using GS_Shop.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GS_Shop.Service
+{
+    public interface IErrorService
+    {
+        Error Add(Error error);
+        void SaveChanges();
+    }
+    public class ErrorService : IErrorService
+    {
+        IErrorRepository _errorRepository;
+        IUnitOfWork _unitOfWork;
+        public ErrorService(IErrorRepository errorRepository, IUnitOfWork unitOfWork)
+        {
+            this._errorRepository = errorRepository; //gán vào biến nội tại và thao tác trong các phương thức
+            this._unitOfWork = unitOfWork;
+        }
+        public Error Add(Error error)
+        {
+            return _errorRepository.Add(error);
+        }
+
+        public void SaveChanges()
+        {
+            _unitOfWork.Commit();
+        }
+    }
+}
